036b0bd93fe186d3a97f90e4766b8c241695d2cd
Peterdu1109 <peter1109@hotmail.fr>
2026-04-13 13:24:31 +0200

    fix(core): refactor FileTransformation registration to IScheduledTask to mimic EditorsChoice precisely

--- FileTransformationService.cs
+++ FileTransformationService.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.Loader;
 using System.Threading;
 using System.Threading.Tasks;
